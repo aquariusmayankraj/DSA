2e52fdf6d6c255d8cb88a1ea8c0f7195c9e4e562
Mayank Raj <ironheart.mayank@gmail.com>
2025-09-10 08:53:39 +0530

    Time: 648 ms (99.85%) | Memory: 0B (100.00%) - LeetSync

--- DSA/1390-average-selling-price/average-selling-price.sql
+++ DSA/1390-average-selling-price/average-selling-price.sql
@@ -1,6 +1,13 @@
-select p.product_id,ifnull(round(sum(p.price*u.units)/sum(u.units),2),0) as average_price
-from Prices p
-left join UnitsSold u
-on p.product_id = u.product_id
-and u.purchase_date between p.start_date and p.end_date
-group by p.product_id
+select p.product_id,
+       ifnull(
+         round(
+           sum(p.price * u.units) / nullif(sum(u.units), 0),
+           2
+         ),
+         0
+       ) as average_price
+from prices p
+left join unitssold u
+  on u.product_id = p.product_id
+ and u.purchase_date between p.start_date and p.end_date
+group by p.product_id;
