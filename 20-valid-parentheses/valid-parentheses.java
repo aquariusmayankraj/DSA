e478ae4b8665ec28a9bdadc28419b9c0722e1532
Mayank Raj <ironheart.mayank@gmail.com>
2026-04-25 12:03:16 +0530

    Time: 3 ms (87.81%) | Memory: 43.3 MB (42.50%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,19 +3,16 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(ch=='('||ch=='{'||ch=='['){
-                stack.push(ch);
+            if(ch=='('){
+                stack.push(')');
             }
-            else if(!stack.isEmpty()&&ch==')'&&stack.peek()=='('){
-                stack.pop();
+            else if(ch=='{'){
+                stack.push('}');
             }
-            else if(!stack.isEmpty()&&ch=='}'&&stack.peek()=='{'){
-                stack.pop();
+            else if(ch=='['){
+                stack.push(']');
             }
-            else if(!stack.isEmpty()&&ch==']'&&stack.peek()=='['){
-                stack.pop();
-            }
-            else{
+            else if(stack.isEmpty()|| stack.pop()!=ch){
                 return false;
             }
         }
