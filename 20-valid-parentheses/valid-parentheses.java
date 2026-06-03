e9bb0796033538d35ce458f49b917ed3ba56a603
Mayank Raj <ironheart.mayank@gmail.com>
2026-06-03 08:21:02 +0530

    Time: 4 ms (39.65%) | Memory: 43.5 MB (25.49%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i=0;i<s.length();i++){
+
+        for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(ch=='('){
+
+            if(ch == '(') {
                 stack.push(')');
             }
-            else if(ch=='{'){
+            else if(ch == '{') {
                 stack.push('}');
             }
-            else if(ch=='['){
+            else if(ch == '[') {
                 stack.push(']');
             }
-            else if(stack.isEmpty()|| stack.pop()!=ch){
+            else if(stack.isEmpty() || stack.pop() != ch) {
                 return false;
             }
         }
+
         return stack.isEmpty();
     }
 }
