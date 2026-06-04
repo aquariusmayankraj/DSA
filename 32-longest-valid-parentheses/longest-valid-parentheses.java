065edaa25eff71b56731758185f7cd41fea84245
Mayank Raj <ironheart.mayank@gmail.com>
2026-06-04 08:23:28 +0530

    Time: 2 ms (97.54%) | Memory: 44.1 MB (98.99%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,22 +1,39 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
-        int maxLen = 0;
+        int left = 0;
+        int right = 0;
+        int MaxLength =0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
-                stack.push(i);
+                left++;
             }
             else{
-                stack.pop();
-                if(stack.isEmpty()){
-                    stack.push(i);
-                }
-                else{
-                    maxLen = Math.max(maxLen,i-stack.peek());
-                }
+                right++;
+            }
+            if(left == right){
+                MaxLength = Math.max(MaxLength, 2*right);
+            }
+            else if(right>left){
+                left=right=0;
             }
         }
-        return maxLen;
+
+        //reset karna hoga pointers ko iske baad yahi main catch hain iss problem ka 
+        left = right = 0;
+        for(int i=s.length()-1;i>0;i--){
+            if(s.charAt(i)=='('){
+                left++;
+            }
+            else{
+                right++;
+            }
+            if(left == right){
+                MaxLength = Math.max(MaxLength, 2*left);
+            }
+            else if(left>right){
+                left=right=0;
+            }
+        }
+        return MaxLength;
     }
 }
