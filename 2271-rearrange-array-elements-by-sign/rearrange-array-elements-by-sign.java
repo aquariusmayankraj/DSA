f90244c1b0082467e298c1e7ed899bf906a251f2
Mayank Raj <ironheart.mayank@gmail.com>
2026-07-20 22:47:57 +0530

    Time: 3 ms (100.00%) | Memory: 175.5 MB (57.39%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -3,13 +3,13 @@ class Solution {
         int[] ans = new int[nums.length];
         int p = 0;
         int n = 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                ans[p] = nums[i];
-                p += 2;
-            } else {
-                ans[n] = nums[i];
-                n += 2;
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i]>0) {
+                ans[p]=nums[i];
+                p+=2;
+            } else{
+                ans[n]=nums[i];
+                n+=2;
             }
         }
         return ans;
