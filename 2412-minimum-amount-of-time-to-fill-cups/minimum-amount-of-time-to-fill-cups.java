48a85fd04fae6f0ca64a0e03eeb15c15fba1c99e
Mayank Raj <ironheart.mayank@gmail.com>
2026-07-27 23:36:56 +0530

    Time: 2 ms (69.19%) | Memory: 43.4 MB (5.62%) - LeetSync

--- 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.java
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.java
@@ -1,14 +1,12 @@
+import java.util.Arrays;
+
 class Solution {
     public int fillCups(int[] amount) {
-        int ans = 0;
-        while (amount[0] != 0 || amount[1] != 0 || amount[2] != 0) {
-            Arrays.sort(amount);
-            if (amount[2] > 0)
-                amount[2]--;
-            if (amount[1] > 0)
-                amount[1]--;
-            ans++;
+        Arrays.sort(amount);
+        if (amount[2] >= amount[0] + amount[1]) {
+            return amount[2];
         }
-        return ans;
+        int sum = amount[0] + amount[1] + amount[2];
+        return (sum + 1) / 2;
     }
 }
