3a096d1054186e3e83350e1766d37f893a7676a0
Mayank Raj <ironheart.mayank@gmail.com>
2026-08-05 23:03:05 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (52.22%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,45 +1,55 @@
 class Solution {
-    public int lastFind(int[] nums, int target){
-        int left = 0;
-        int right = nums.length-1;
-        int ans = -1;
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if(nums[mid]<target){
-                left = mid+1;
-            }
-            else if(nums[mid]==target){
-                ans = mid;
-                left = mid+1;
-            }
-            else{
-                right = mid-1;
-            }
-        }
-        return ans;
-    }
-    public int firstFind(int[] nums, int target){
+
+    public int firstFind(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
         int ans = -1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(nums[mid]==target){
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
                 ans = mid;
-                high = mid-1;
-            }
-            else if(nums[mid]<target){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
+                high = mid - 1;   // Left side search
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
             }
         }
+
         return ans;
     }
+
+    public int lastFind(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;    // Right side search
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
     public int[] searchRange(int[] nums, int target) {
         int first = firstFind(nums, target);
         int last = lastFind(nums, target);
+
         return new int[]{first, last};
     }
 }
